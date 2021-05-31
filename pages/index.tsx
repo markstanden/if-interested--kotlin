b56ca08417398c5d9efc770b18ea18624fcc029c
Mark Standen <mark@markstanden.dev>
2021-05-31 10:13:47 +0100

    updated to next.js 10 and swapped sass to straight css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import NavMenu from '../components/navMenu'
 
-function HomePage() {
+function Home() {
   return (
     <>
       <Header />
@@ -14,4 +14,4 @@ function HomePage() {
   )
 }
 
-export default HomePage
+export default Home
