245c670dd53bc5e7fda154ace6a1910a9cbdaea4
Mark Standen <mark@markstanden.dev>
2021-06-01 11:11:32 +0100

    work on nav

--- components/NavButton.jsx
+++ components/NavButton.jsx
@@ -1,17 +1,53 @@
-import styles from './NavButton.module.css'
+// import styles from './NavButton.module.css'
 
 function NavButton() {
   return (
-    <>
-      <div className={styles.navButtonContainer}>
-        <div className={styles.navButtonClip}>
-          <div className={styles.navButton}>
-            <div className={`${styles.navButton} ${styles.navButton___Red}`}></div>
-          </div>
-        </div>
-        <div className={styles.navButtonText}>Menu</div>
+    <button className="navButtonContainer">
+      <div className="navButton">
+        <div className="navButtonText">Menu</div>
       </div>
-    </>
+
+      <style jsx>{`
+        --navButton-size: calc(var(--navHeight) * 0.8);
+
+        .navButtonContainer {
+          border: none;
+          position: fixed;
+          bottom: calc(var(--navButton-size) / 2);
+          transform: translate(0%, 50%);
+          right: var(--navButton-position-right);
+          background-color: transparent;
+          z-index: 103;
+          height: var(--navButton-size);
+          width: var(--navButton-size));
+          cursor: pointer;
+        }
+
+        .navButton {
+          position: relative;
+          top: 0;
+          right: 0;
+          height: var(--navButton-size);
+          width: var(--navButton-size);
+          border-radius: 50%;
+          z-index: 105;
+          overflow: hidden;
+          background-color: var(--color-secondary);
+        }
+
+        .navButtonText {
+          position: relative;
+          top: calc(var(--navButton-size) /4);
+          width: var(--navButton-size);
+          padding-top: 0.25rem;
+          font-family: var(font-hero);
+          font-size: 1.3rem;
+          text-align: center;
+          color: red;
+          text-shadow: 1px 1px 4px black;
+        }
+      `}</style>
+    </button>
   )
 }
 
--- components/NavMenu.jsx
+++ components/NavMenu.jsx
@@ -1,15 +1,74 @@
-import styles from './NavMenu.module.css'
+// import styles from './NavMenu.module.css'
 
 function NavMenu(props) {
   return (
-    <nav className={`${styles.navMenu} ${props.visible ? styles.navMenu___isVisible : ''}`}>
-      <ul className={styles.navMenu_list}>
-        <li className={styles.navMenu_list_items}>Home</li>
-        <li className={styles.navMenu_list_items}>Blog</li>
-        <li className={styles.navMenu_list_items}>Stack</li>
-        <li className={styles.navMenu_list_items}>Markdown</li>
-        <li className={styles.navMenu_list_items}>Github</li>
-        <li className={styles.navMenu_list_items}>Author</li>
+    <nav className={`navMenu ${props.visible ? `navMenu___isVisible` : ''}`}>
+      <style jsx>
+        {`
+          .navMenu {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+
+            font-size: 2rem;
+            color: var(--color-primary);
+            background: var(--nav-gradient);
+
+            border-top: 5px solid;
+
+            height: 0;
+            opacity: 0;
+            z-index: -1;
+            transition: height 0.75s ease-in-out 0.4s, z-index 1s ease-in-out 1s, opacity 1s ease-in-out 1s;
+          }
+
+          .navMenu___isVisible {
+            height: var(--navHeight);
+            opacity: 1;
+            z-index: 10;
+            transition: height 0.75s ease-in-out 0.2s, z-index 1s ease-in-out, opacity 1s;
+          }
+
+          .navMenu_list {
+            opacity: 0;
+            transition: opacity 0.5s ease-in-out;
+            max-width: 80%;
+            margin-top: 2.2rem;
+            margin-bottom: 1rem;
+            margin-left: 5rem;
+            margin-right: 5rem;
+            display: flex;
+            flex-basis: content;
+            justify-content: space-around;
+            align-items: center;
+            list-style-type: none;
+          }
+
+          .navMenu_list_items {
+            font-size: 1.5rem;
+            font-weight: 300;
+            text-shadow: 0.5px 0.5px 8px black;
+          }
+
+          .navMenu___isVisible .navMenu_list {
+            opacity: 1;
+            transition: opacity 0.5s ease-in-out 0.3s;
+          }
+
+          .navMenu_list :not(:last-child) {
+            padding-right: $item-spacing;
+          }
+        `}
+      </style>
+
+      <ul className="navMenu_list">
+        <li className="navMenu_list_items">Home</li>
+        <li className="navMenu_list_items">Blog</li>
+        <li className="navMenu_list_items">Stack</li>
+        <li className="navMenu_list_items">Markdown</li>
+        <li className="navMenu_list_items">Github</li>
+        <li className="navMenu_list_items">Author</li>
       </ul>
     </nav>
   )
